279faf9e135b306117168350d6ab7d0177db90dd
RenixExtreme <ivanrene10@gmail.com>
2021-10-26 19:06:57 -0500

    Update clientetotemGUI.java

--- ClienteTotem/src/com/foodsquare/clientetotemGUI.java
+++ ClienteTotem/src/com/foodsquare/clientetotemGUI.java
@@ -1,8 +1,16 @@
 package com.foodsquare;
 
 import java.awt.EventQueue;
+import java.awt.Image;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
+import javax.imageio.ImageIO;
+import javax.swing.Icon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.BorderLayout;
 
 public class clientetotemGUI {
 
@@ -26,18 +34,38 @@ public class clientetotemGUI {
 
 	/**
 	 * Create the application.
+	 * @throws MalformedURLException 
 	 */
-	public clientetotemGUI() {
+	public clientetotemGUI() throws MalformedURLException {
 		initialize();
 	}
 
 	/**
 	 * Initialize the contents of the frame.
 	 */
-	private void initialize() {
+	private void initialize() throws MalformedURLException {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		frame.getContentPane().add(lblNewLabel, BorderLayout.CENTER);
+		
+		
+		URL url;
+		try {
+			url = new URL("https://s3.us-west-1.amazonaws.com/files-muy/photos/menus/dishes/thumbnails/Arroz_Con_Pollo_Kraft_Col.png");
+
+			Image image = ImageIO.read(url);
+			
+			lblNewLabel.setIcon((Icon) image);
+			
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 
 }
